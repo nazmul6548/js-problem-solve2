b9ef8a1b92371b00df473127d4de35a27acc653f
nazmul6548 <nazmul6548@gmail.com>
2023-11-06 20:22:51 +0600

    2nd ans

--- index1.js
+++ index1.js
@@ -1,2 +1,4 @@
+// problem2:How can we arrange the names of everyone in our classroom alphabetically?
+// _______ans________
 const allStudentName =['tamim','riyad','ashraful','mustafiz','shakib','zahir','taskin','towhid'];
 console.log(allStudentName.sort());
