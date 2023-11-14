6370edd7dd8785039c847b9a6ee8f71079516750
nazmul6548 <nazmul6548@gmail.com>
2023-11-14 15:58:29 +0600

    spread operators

---
+++ index9.js
@@ -0,0 +1,14 @@
+// general way
+const myArry = [2,23,53,5];
+const myArry1 = [9,8,7,3];
+const newArray = myArry.flat()
+console.log(newArray);
+
+
+// spread operators_____
+// const myArry3=[9,76,5,1,2];
+// const myArry4=[7,8,4,3];
+// const result =[...myArry3,...myArry4];
+// console.log(result);
+
+
